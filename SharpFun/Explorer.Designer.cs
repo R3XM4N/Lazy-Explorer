fc7982b3eb9861364096d8efd51eb6a4958609ec
Rex <trolove321@gmail.com>
2024-10-15 10:49:05 +0200

    Add project files.

---
+++ SharpFun/Explorer.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace SharpFun
+{
+    partial class Explorer
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            toolStrip1 = new ToolStrip();
+            listBox1 = new ListBox();
+            listBox2 = new ListBox();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            toolStrip1.BackColor = SystemColors.ControlDarkDark;
+            toolStrip1.ImageScalingSize = new Size(20, 20);
+            toolStrip1.Location = new Point(0, 0);
+            toolStrip1.Name = "toolStrip1";
+            toolStrip1.Size = new Size(1076, 25);
+            toolStrip1.TabIndex = 0;
+            toolStrip1.Text = "toolStrip1";
+            // 
+            // listBox1
+            // 
+            listBox1.BackColor = SystemColors.ActiveBorder;
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 23;
+            listBox1.Location = new Point(31, 76);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(443, 395);
+            listBox1.TabIndex = 1;
+            listBox1.DoubleClick += listBox1_DoubleClick;
+            // 
+            // listBox2
+            // 
+            listBox2.BackColor = SystemColors.ActiveBorder;
+            listBox2.FormattingEnabled = true;
+            listBox2.ItemHeight = 23;
+            listBox2.Location = new Point(612, 76);
+            listBox2.Name = "listBox2";
+            listBox2.Size = new Size(443, 395);
+            listBox2.TabIndex = 2;
+            listBox2.DoubleClick += listBox2_DoubleClick;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(31, 40);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(443, 30);
+            textBox1.TabIndex = 3;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(612, 40);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(443, 30);
+            textBox2.TabIndex = 4;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // Explorer
+            // 
+            AutoScaleDimensions = new SizeF(11F, 23F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlDarkDark;
+            ClientSize = new Size(1076, 528);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(listBox2);
+            Controls.Add(listBox1);
+            Controls.Add(toolStrip1);
+            Font = new Font("Arial", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Margin = new Padding(4);
+            Name = "Explorer";
+            Text = "Explorer";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ToolStrip toolStrip1;
+        private ListBox listBox1;
+        private ListBox listBox2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+    }
+}
---
+++ SharpFun/Explorer.cs
@@ -0,0 +1,110 @@
+﻿using Explorer;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace SharpFun
+{
+    public partial class Explorer : Form
+    {
+        List<Displayed> leftBox = new List<Displayed>();
+        List<Displayed> rightBox = new List<Displayed>();
+
+        public Explorer()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            updateExploredDirs(@"C:\", leftBox, listBox1, textBox1);
+            updateExploredDirs(@"C:\", rightBox, listBox2, textBox2);
+        }
+
+        void Dirinfo(string path, List<Displayed> box, ListBox listBox, TextBox? textbox)
+        {
+            if (textbox != null)
+            {
+                textbox.Text = path;
+            }
+            List<string> paths = Directory.GetDirectories(path).Concat(Directory.GetFiles(path)).ToList<string>();
+            //string names = new List<string>();
+            box.Clear();
+            foreach (string item in paths)
+            {
+                box.Add(new Displayed("tets", item));
+            }
+            UpdateDisp(listBox, box);
+        }
+        void UpdateDisp(ListBox listBox, List<Displayed> displayeds)
+        {
+            listBox.Items.Clear();
+            listBox.Items.Add("↵");
+            foreach (Displayed disp in displayeds)
+            {
+                listBox.Items.Add(disp.PathWay);
+            }
+        }
+        void updateExploredDirs(String pathway, List<Displayed> displayeds, ListBox listBox,TextBox? textBox)
+        {
+            try
+            {
+                if (Directory.Exists(pathway))
+                {
+                    if (textBox1 != null)
+                    {
+
+                        Dirinfo(pathway, displayeds, listBox, textBox);
+                    }
+                    else
+                    {
+                        Dirinfo(pathway, displayeds, listBox, null);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            updateExploredDirs(textBox1.Text, leftBox, listBox1,null);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            updateExploredDirs(textBox2.Text, rightBox, listBox2,null);
+        }
+
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            doubleClickBox(listBox1.Text, leftBox, listBox1,textBox1);
+        }
+
+        private void listBox2_DoubleClick(object sender, EventArgs e)
+        {
+            doubleClickBox(listBox2.Text, rightBox, listBox2, textBox2);
+        }
+
+        void doubleClickBox(String pathway, List<Displayed> displayeds, ListBox listBox, TextBox? textBox)
+        {
+            if (listBox.SelectedIndex == 0)
+            {
+                if (Directory.GetDirectoryRoot(textBox.Text) != textBox.Text)
+                {
+                    updateExploredDirs(Directory.GetParent(textBox.Text).FullName, displayeds, listBox, textBox);
+                }
+            }
+            else if (Directory.Exists(pathway))
+            {
+                updateExploredDirs(pathway, displayeds, listBox, textBox);
+            }
+            else if (File.Exists(pathway))
+            {
+                //Filezies
+            }
+        }
+    }
+}
